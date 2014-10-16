136d6e90627e7fa75647f3a6e4d814da0e4685b3
Richard Alam <ritzalam@gmail.com>
2014-10-16 00:30:55 -0700

    - trying out how to send event

--- examples/status/status.js
+++ examples/status/status.js
@@ -3,10 +3,19 @@
 var esl = require('modesl'),
 //open a connection
 conn = new esl.Connection('127.0.0.1', 8021, 'ClueCon', function() {
+    conn.api("conference list", function(res) {
+      console.log(res.getBody());
+    })
     //send the status api command
-    conn.api('status', function(res) {
+    conn.events('plain', 'all', function(res) {
         //log result body and exit
-        console.log(res.getBody());
-        process.exit(0);
+        console.log("got: \n" + res.getBody());
     });
 });
+
+console.log("Listening for events.");
+
+conn.on('esl::event::**', function(e) {
+  console.log(e);
+});
+
